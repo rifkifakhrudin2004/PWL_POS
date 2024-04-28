d556df54365f0e280f0e2010f9f554bd558fc18e
rifkifakhrudin2004 <ashiap1927@gmail.com>
2024-04-28 15:34:48 +0700

    Praktikum 1 Jb 10

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,21 +1,24 @@
 <?php
 
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Relations\BelongsTo;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Contracts\Auth\Authenticatable;
+// use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
-
-    protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
-
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    public function getJWTIdentifier(){
+        return $this->getkey();
     }
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
+    protected $table ='m_user';
+    protected $primarykey ='user_id';
+    protected $fillable =['level_id','username','nama','password'];
 }
--- config/auth.php
+++ config/auth.php
@@ -40,7 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
     ],
+],
 
     /*
     |--------------------------------------------------------------------------
@@ -62,7 +66,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\UserModel::class,
         ],
 
         // 'users' => [
